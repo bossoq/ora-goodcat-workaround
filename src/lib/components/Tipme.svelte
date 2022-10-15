0f9115c2956dacd1b77b520d2b9b7f32d9ede46e
Kittipos Wajakajornrit <boss.zz@me.com>
2022-10-15 12:34:59 +0700

    Add select-none to tipme

--- src/lib/components/Tipme.svelte
+++ src/lib/components/Tipme.svelte
@@ -4,7 +4,7 @@
   const baseUrl = 'https://tipme.in.th/'
 </script>
 
-<div class="absolute bottom-0 left-0 m-4 text-sm sm:text-base">
+<div class="select-none absolute bottom-0 left-0 m-4 text-sm sm:text-base">
   <a href={baseUrl + name} target="_blank">
     <button class="rounded-2xl p-2 bg-red-600 text-black dark:text-white">{label}</button>
   </a>
