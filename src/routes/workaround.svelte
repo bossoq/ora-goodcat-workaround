7dc5e53cd726fef1262271aacd2f54c2a0517b12
Kittipos Wajakajornrit <boss.zz@me.com>
2022-10-15 16:53:24 +0700

    Change new layout from table to bullet

--- src/routes/workaround.svelte
+++ src/routes/workaround.svelte
@@ -28,34 +28,31 @@
     bind:value={search}
     class="border border-gray-300 dark:border-gray-500 bg-gray-200 dark:bg-white rounded-lg text-gray-800 p-2 my-4 w-full"
   />
-  <div class="h-5/6 overflow-x-auto">
-    <table class="min-w-full">
-      <thead class="font-bold align-text-top bg-teal-400 dark:bg-teal-800 sticky top-0">
-        <tr>
-          <th class="py-2 px-1 w-auto sm:w-36">หัวข้อ</th>
-          <th class="py-2 px-1 w-1/3">อาการ</th>
-          <th class="py-2 px-1 w-auto min-w-fit">วิธีแก้</th>
-        </tr>
-      </thead>
-      <tbody class="align-text-top bg-teal-200 dark:bg-teal-600">
-        {#each visibleWorkarounds as work}
-          <tr class="border-b border-gray-50">
-            <td class="py-3 px-1">{work.Title}</td>
-            <td class="py-3 px-1">
-              {#each work.Symptom.split('\n') as line}
-                - {line}
-                {@html '<br>'}
-              {/each}
-            </td>
-            <td class="py-3 px-1">
-              {#each work.Workaround.split('\n') as line}
-                - {line}
-                {@html '<br>'}
-              {/each}
-            </td>
-          </tr>
-        {/each}
-      </tbody>
-    </table>
+  <div class="h-5/6 overflow-x-auto scrollbar-hide">
+    {#each visibleWorkarounds as work}
+      <div class="py-1">
+        <h2 class="underline font-bold text-base sm:text-xl">{work.Title}</h2>
+        <div
+          class="flex flex-row gap-6 sm:gap-8 text-gray-800 dark:text-gray-200 text-sm sm:text-lg pb-1"
+        >
+          <p class="font-medium w-16 shrink-0">อาการ</p>
+          <ul class="list-disc">
+            {#each work.Symptom.split('\n') as symptom}
+              <li>{symptom}</li>
+            {/each}
+          </ul>
+        </div>
+        <div
+          class="flex flex-row gap-6 sm:gap-8 text-gray-800 dark:text-gray-200 text-sm sm:text-lg pb-1"
+        >
+          <p class="font-medium w-16 shrink-0">วิธีแก้</p>
+          <ul class="list-disc">
+            {#each work.Workaround.split('\n') as workaroundLine}
+              <li>{workaroundLine}</li>
+            {/each}
+          </ul>
+        </div>
+      </div>
+    {/each}
   </div>
 </div>
