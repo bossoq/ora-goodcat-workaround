7dfbe2a668cb6475ab546588e0b2665885b55dac
Kittipos Wajakajornrit <boss.zz@me.com>
2023-05-26 13:59:45 +0700

    Update parsing URL

--- src/routes/dataparser/+page.ts
+++ src/routes/dataparser/+page.ts
@@ -3,6 +3,6 @@ import { redirect } from '@sveltejs/kit'
 export const load = () => {
   throw redirect(
     302,
-    'https://script.google.com/macros/s/AKfycbyQEYOdFP6JcEKcw19rNCOLdFQ2NjFh_QCB9g4ZTeU0bfMlWxMM1RMXf34f9NoixSSa/exec'
+    'https://script.google.com/macros/s/AKfycbySTFJ7mSQ2gOktETpU1KplR05zVAq97iI8YpEwBvQZ45dk3w0rRsVzGJ3gnDMXM-tz/exec'
   )
 }
